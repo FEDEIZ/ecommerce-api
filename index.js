526432b82500a1eb7e393dbcd841aabee1638a68
FEDEIZ <ing.finterlandi@gmail.com>
2023-02-07 09:32:52 -0300

    Revert "Revert "prueba para aws""
    This reverts commit a127a988a5189edb59e6a5a360f36862499d1dd5.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello!");
+const server = require("./src/app.js");
+//const { conn } = require("./src/db.js");
+//const loadDb = require("./src/loadDb.js");
+//require("dotenv").config();
+//const { PORT } = process.env;
+
+const fs = require("fs");
+//const https = require("https");
+const http = require("http");
+
+//const privateKey = fs.readFileSync("./sslcert/private.key", "utf8");
+//const certificate = fs.readFileSync("./sslcert/certificate.crt", "utf8");
+//const credentials = { key: privateKey, cert: certificate };
+
+//const httpsServer = https.createServer(credentials, server);
+const httpServer = http.createServer(server);
+
+const startServer = () => {
+  httpServer.listen(3001, async () => {
+    console.log("Listening at PORT " + 3001);
+    //await loadDb();
+  });
+};
+
+// const startDB = async () => {
+//   return await conn.sync({ force: true });
+// };
+
+const start = async () => {
+  //await startDB();
+  startServer();
+};
+
+start();
